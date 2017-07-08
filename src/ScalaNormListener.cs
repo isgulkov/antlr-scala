505a9c2d1516cac92177d334ea9b6aacdc1074c9
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-08 15:43:51 +0300

    Print expressions in such a way that bitwise ops have high precedence
    Unfortunately, in C#, unlike Scala, bitwise operators have low precedence. Hence all the recursive expression printing just to put bitwise expressions in parentheses

--- src/ScalaNormListener.cs
+++ src/ScalaNormListener.cs
@@ -36,6 +36,59 @@ namespace ScalaTranslator
 			Environment.Exit(1);
 		}
 
+		static string PrintExpression(ScalaParser.ExpressionContext context)
+		{
+			if(context.expression() != null) {
+				return $"{PrintExpression(context.comparativeExpr())} = {PrintExpression(context.expression())}";
+			}
+			else {
+				return PrintExpression(context.comparativeExpr());
+			}
+		}
+
+		static string PrintExpression(ScalaParser.ComparativeExprContext context)
+		{
+			if(context.comparativeExpr() != null) {
+				return $"{PrintExpression(context.comparativeExpr())}" +
+					$" {context.children[1].GetText()} {PrintExpression(context.multiplicativeExpr())}";
+			}
+			else {
+				return PrintExpression(context.multiplicativeExpr());
+			}
+		}
+
+		static string PrintExpression(ScalaParser.MultiplicativeExprContext context)
+		{
+			if(context.multiplicativeExpr() != null) {
+				return $"{PrintExpression(context.multiplicativeExpr())}" +
+					$" {context.children[1].GetText()} {PrintExpression(context.bitshiftExpr())}";
+			}
+			else {
+				return PrintExpression(context.bitshiftExpr());
+			}
+		}
+
+		static string PrintExpression(ScalaParser.BitshiftExprContext context)
+		{
+			if(context.bitshiftExpr() != null) {
+				return $"({PrintExpression(context.bitshiftExpr())}" +
+					$" {context.children[1].GetText()} {PrintExpression(context.primaryExpr())})";
+			}
+			else {
+				return PrintExpression(context.primaryExpr());
+			}
+		}
+
+		static string PrintExpression(ScalaParser.PrimaryExprContext context)
+		{
+			if(context.expression() != null) {
+				return $"({PrintExpression(context.expression())})";
+			}
+			else {
+				return context.GetText();
+			}
+		}
+
 		public override void EnterFile(ScalaParser.FileContext context)
 		{
             OutLine("using System;");
@@ -63,13 +116,13 @@ namespace ScalaTranslator
 				OutLine($"dynamic {context.ID().GetText()};");
 			}
 			else {
-				OutLine($"dynamic {context.ID().GetText()} = {context.expression().GetText()};");
+				OutLine($"dynamic {context.ID().GetText()} = {PrintExpression(context.expression())};");
 			}
 		}
 
 		public override void EnterWhileStmt(ScalaParser.WhileStmtContext context)
 		{
-			OutLine($"while({context.expression().GetText()}) {{");
+            OutLine($"while({PrintExpression(context.expression())}) {{");
 		}
 
 		public override void ExitWhileStmt(ScalaParser.WhileStmtContext context)
@@ -81,7 +134,7 @@ namespace ScalaTranslator
 		{
 			Out($"foreach(var {context.ID().GetText()} in ");
 
-			Out($"Range({context.expression()[0].GetText()}, {context.expression()[1].GetText()})");
+            Out($"Range({PrintExpression(context.expression()[0])}, {PrintExpression(context.expression()[1])})");
 
 			OutLine(") {");
 		}
@@ -94,14 +147,14 @@ namespace ScalaTranslator
 		public override void EnterPrintStmt(ScalaParser.PrintStmtContext context)
 		{
 			if(context.expression() != null) {
-				OutLine($"Console.Write({context.expression().GetText()});");
+                OutLine($"Console.Write({PrintExpression(context.expression())});");
 			}
 		}
 
 		public override void EnterPrintlnStmt(ScalaParser.PrintlnStmtContext context)
 		{
 			if(context.expression() != null) {
-				OutLine($"Console.WriteLine({context.expression().GetText()});");
+                OutLine($"Console.WriteLine({PrintExpression(context.expression())});");
 			}
 			else {
                 OutLine("Console.WriteLine();");
@@ -111,7 +164,7 @@ namespace ScalaTranslator
 		public override void EnterStatement(ScalaParser.StatementContext context)
 		{
 			if(context.expression() != null) {
-				OutLine($"{context.expression().GetText()};");
+				OutLine($"{PrintExpression(context.expression())};");
 			}
 		}
 	}
