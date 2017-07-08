550a713cefb61770f4e33516f214b663bd78fe07
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-08 15:01:55 +0300

    Add a stub for the translator project

---
+++ gen.sh
@@ -0,0 +1,1 @@
+java -jar antlr-4.7-complete.jar Scala.g4 -o src
---
+++ src/ScalaNormListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace ScalaTranslator
+{
+	public class ScalaNormListener : ScalaBaseListener
+	{
+		readonly string OutputFilename;
+		StreamWriter OutputWriter;
+
+		public ScalaNormListener(string outputFilename)
+		{
+			OutputFilename = outputFilename;
+
+			OutputWriter = new StreamWriter(File.Open(OutputFilename, FileMode.Create));
+			OutputWriter.AutoFlush = true;
+		}
+
+		void Out(string s)
+		{
+			OutputWriter.Write(s);
+		}
+
+		void OutLine(string s)
+		{
+			OutputWriter.WriteLine(s);
+		}
+
+		void PrintErrorAndExit(int error_code, string message)
+		{
+			Console.WriteLine($"Error {error_code}: {message}");
+
+			OutputWriter.Close();
+			File.Delete(OutputFilename);
+
+			Environment.Exit(1);
+		}
+	}
+}
