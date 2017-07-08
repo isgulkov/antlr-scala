4e74d1e6fd23cc80a3d6a9f284bd98806b8ff1cb
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-08 15:23:14 +0300

    Print several kinds of statements...

--- src/ScalaNormListener.cs
+++ src/ScalaNormListener.cs
@@ -35,5 +35,76 @@ namespace ScalaTranslator
 
 			Environment.Exit(1);
 		}
+
+		public override void EnterFile(ScalaParser.FileContext context)
+		{
+            OutLine("using System;");
+			OutLine("using System.Linq;");
+			OutLine("");
+			OutLine("class Program {");
+            OutLine("static IEnumerable<int> Range(long start, long end)");
+			OutLine("{");
+			OutLine("return Enumerable.Range(start, end - start + 1);");
+			OutLine(")");
+			OutLine("");
+            OutLine("public static void Main() {");
+		}
+
+		public override void ExitFile(ScalaParser.FileContext context)
+		{
+			OutLine("}");
+            OutLine("}");
+		}
+
+		public override void EnterDeclarationStmt(ScalaParser.DeclarationStmtContext context)
+		{
+			if(context.expression() == null) {
+				OutLine($"object {context.ID().GetText()};");
+			}
+			else {
+				OutLine($"var {context.ID().GetText()} = {context.expression().GetText()};");
+			}
+		}
+
+		public override void EnterWhileStmt(ScalaParser.WhileStmtContext context)
+		{
+			OutLine($"while({context.expression().GetText()}) {{");
+		}
+
+		public override void ExitWhileStmt(ScalaParser.WhileStmtContext context)
+		{
+			OutLine("}");
+		}
+
+		public override void EnterForStmt(ScalaParser.ForStmtContext context)
+		{
+			Out($"foreach(var {context.ID().GetText()} in ");
+
+			Out($"Enumerable.Range({context.expression()[0]}, {context.expression()[0]})");
+
+			OutLine(") {");
+		}
+
+		public override void ExitForStmt(ScalaParser.ForStmtContext context)
+		{
+			OutLine("}");
+		}
+
+		public override void EnterPrintStmt(ScalaParser.PrintStmtContext context)
+		{
+			if(context.expression() != null) {
+				OutLine($"Console.Write({context.expression().GetText()});");
+			}
+		}
+
+		public override void EnterPrintlnStmt(ScalaParser.PrintlnStmtContext context)
+		{
+			if(context.expression() != null) {
+				OutLine($"Console.WriteLine({context.expression().GetText()});");
+			}
+			else {
+                OutLine("Console.WriteLine();");
+			}
+		}
 	}
 }
